6efc0b43ce7c44c3c7282c12fedb6772803167b7
Luiz451 <luiz.fernando.sf.2006@gmail.com>
2026-03-28 12:33:09 -0300

    Adicionar estrutura inicial do projeto com arquivos de configuração e dependências do Spring Boot

---
+++ src/main/java/com/piec1/api_iot/ApiIotApplication.java
@@ -0,0 +1,13 @@
+package com.piec1.api_iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIotApplication.class, args);
+	}
+
+}
